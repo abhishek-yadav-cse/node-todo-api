121df915fdb3d9028fe60b231fda413befbf2909
Abhishek Yadav <abhishek.yadav.cs@gmail.com>
2018-12-12 23:19:00 -0800

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -137,9 +137,6 @@ UserSchema.statics.findByToken = function (token) {
   try {
     decoded = jwt.verify(token, 'abc123');
   } catch (e) {
-    // return new Promise((resolve, reject) => {
-    //   reject();
-    // });
 
     return Promise.reject();
   }
